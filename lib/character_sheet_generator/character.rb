f1ba5bfb211d797dfeebb25a3954f8bfba82f8a0
Caroline <caroline@curieos.com>
2020-01-10 23:30:10 -0600

    Add name attribute to the character class

--- lib/character_sheet_generator/character.rb
+++ lib/character_sheet_generator/character.rb
@@ -1,3 +1,7 @@
 class CharacterSheetGenerator::Character
-	
+	attr_reader :name
+
+	def initialize(name)
+		@name = name
+	end
 end
--- spec/character_sheet_character_spec.rb
+++ spec/character_sheet_character_spec.rb
@@ -1,7 +1,14 @@
 RSpec.describe "Character" do
-	let(:character) { CharacterSheetGenerator::Character.new() }
+	let(:character) { CharacterSheetGenerator::Character.new("Dummy Name") }
 
 	it "exists" do
 		expect(character).to_not be_nil
+	end
+
+	it "has a name" do
+		dummy_name = "Tim"
+		new_character = CharacterSheetGenerator::Character.new(dummy_name)
+
+		expect(new_character.name).to eq(dummy_name)
 	end
 end
