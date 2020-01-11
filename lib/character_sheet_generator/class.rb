0518a0227a817f0103b5fb94a45de618b9131449
Caroline <caroline@curieos.com>
2020-01-11 05:33:15 -0600

    Add class level

--- lib/character_sheet_generator/class.rb
+++ lib/character_sheet_generator/class.rb
@@ -1,7 +1,8 @@
 class CharacterSheetGenerator::Class
-	attr_reader :name
+	attr_reader :name, :level
 
 	def initialize(name)
 		@name = name
+		@level = 1
 	end
 end
--- spec/character_sheet_class_spec.rb
+++ spec/character_sheet_class_spec.rb
@@ -8,4 +8,12 @@ RSpec.describe "Class" do
 	it "has a name" do
 		expect(dummy_class.name).to eq("Dummy Name")
 	end
+
+	it "has a level" do
+		expect(dummy_class.level)
+	end
+
+	it "sets level to 1" do
+		expect(dummy_class.level).to eq(1)
+	end
 end
