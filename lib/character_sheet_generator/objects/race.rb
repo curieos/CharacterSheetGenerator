c70ed1d74d3778c610090c1940ea1698c2b110eb
Caroline <caroline@curieos.com>
2020-01-20 22:06:07 -0600

    Add a speed attribute to the race

--- lib/character_sheet_generator/objects/race.rb
+++ lib/character_sheet_generator/objects/race.rb
@@ -1,9 +1,11 @@
 class CharacterSheetGenerator::Race
-	attr_reader :name, :subrace, :features
+	attr_reader :name, :speed, :subrace, :features
 
-	def initialize(_name, _features, _subrace: nil)
+	def initialize(_name, _speed, _features, _subrace: nil)
 		@name = _name
 		@subrace = _subrace if _subrace != nil
+
+		@speed = _speed
 		@features = _features
 	end
 end
--- spec/objects/race_spec.rb
+++ spec/objects/race_spec.rb
@@ -1,5 +1,5 @@
 RSpec.describe "Race" do
-	let(:dummy_race) { CharacterSheetGenerator::Race.new("Elf", [CharacterSheetGenerator::RaceFeature::new("Dark Vision", "")], _subrace: "High Elf") }
+	let(:dummy_race) { CharacterSheetGenerator::Race.new("Elf", 30, [CharacterSheetGenerator::RaceFeature::new("Dark Vision", "")], _subrace: "High Elf") }
 
 	it "exists" do
 		expect(dummy_race).to_not be_nil
@@ -7,6 +7,10 @@ RSpec.describe "Race" do
 
 	it "has a name" do
 		expect(dummy_race.name).to eq("Elf")
+	end
+
+	it "has a speed" do
+		expect(dummy_race.speed).to eq(30)
 	end
 
 	it "has an optional subrace" do
